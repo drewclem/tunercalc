95ff097bb9c6ec454bdf04f6ce00dbd8cb6c6e2c
Drew Clements <drewclementsdesign@gmail.com>
2025-08-27 17:47:57 -0400

    init

---
+++ resources/js/components/AppLogo.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import AppLogoIcon from '@/components/AppLogoIcon.vue';
+</script>
+
+<template>
+    <div class="flex aspect-square size-16 items-center justify-center">
+        <AppLogoIcon />
+    </div>
+    <p class="sr-only">TunerCalc</p>
+</template>
---
+++ resources/js/pages/Welcome.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import MainLayout from '@/layouts/MainLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Welcome">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+    <MainLayout> testing </MainLayout>
+</template>
